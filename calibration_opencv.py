3d2dbe854de25591d67e5ecafd81a75a2f0dd0c7
Seongoh Ryoo <seongohr@usc.edu>
2020-10-26 14:41:24 -0700

    modify typo

--- calibration_opencv.py
+++ calibration_opencv.py
@@ -11,21 +11,21 @@ objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
-path_images = glob.glob('./cal_picture/*.jpg')  # path to images
-path_one_image = './cal_picture/capture4.jpg'
+images = glob.glob('../april_tag/aprilTag/cal_picture/*.jpg')  # path to images
+path_one_image = '../april_tag/aprilTag/cal_picture/capture4.jpg'
 
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
     # If found, add object points, image points (after refining them)
     if ret is True:
         objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners)
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
         cv.imshow('img', img)
         cv.waitKey(500)
 cv.destroyAllWindows()
